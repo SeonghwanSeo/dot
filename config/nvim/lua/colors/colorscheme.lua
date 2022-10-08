aa2297e5b8514719b9b624b3dddf84bd030a4dba
SeonghwanSeo <shwan0106@gmail.com>
2022-10-08 18:07:43 +0900

    update nvim rendering / color

--- config/nvim/lua/colors/colorscheme.lua
+++ config/nvim/lua/colors/colorscheme.lua
@@ -3,6 +3,7 @@
 --vim.cmd [[colorscheme gruvbox-material]]
 
 vim.opt.termguicolors = true
-vim.cmd[[colorscheme tokyonight]]
-vim.cmd[[highlight MatchParen gui=underline,bold]]
+vim.cmd [[colorscheme tokyonight]]
+vim.cmd [[highlight MatchParen gui=underline,bold]]
+vim.cmd [[highlight NvimTreeFolderIcon guifg=deepskyblue]]
 --vim.cmd [[highlight CursorLine gui=underline cterm=underline]]
--- config/nvim/lua/plugins/nvim_tree.lua
+++ config/nvim/lua/plugins/nvim_tree.lua
@@ -1,3 +1,21 @@
 -- empty setup using defaults
-require("nvim-tree").setup()
 
+require("nvim-tree").setup({
+  renderer = {
+    icons = {
+      webdev_colors = false,
+      show = {
+        file = false,
+        folder_arrow=false,
+      },
+      git_placement = 'after',
+      glyphs = {
+        git = {
+          untracked = '+',
+          ignored = ' ',
+          deleted = '-',
+        }
+      },
+    }
+  }  
+})
