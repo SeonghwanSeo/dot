bf1e8713f912cf87508f0419965102da33a93e7b
SeonghwanSeo <shwan0106@gmail.com>
2022-10-14 19:53:21 +0900

    autocmd

--- config/nvim/lua/autocmds.lua
+++ config/nvim/lua/autocmds.lua
@@ -2,3 +2,4 @@
 vim.api.nvim_exec([[autocmd FileType python nnoremap <C-i> :w<CR>:!python %<CR>]], false)
 vim.api.nvim_exec([[autocmd FileType python set tabstop=4 shiftwidth=4]], false)
 vim.api.nvim_exec([[autocmd User Startified setlocal cursorline]], false)
+vim.api.nvim_exec([[autocmd TermOpen * set nonu]], false)
