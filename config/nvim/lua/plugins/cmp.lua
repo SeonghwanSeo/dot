e5fc64751c8c34410411b4be4ef4f8216c7ab82e
Seonghwan Seo <shwan0106@kaist.ac.kr>
2022-11-30 16:05:57 +0900

    update

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -35,11 +35,19 @@ cmp.setup({
         get_bufnrs = function()
           local buf = vim.api.nvim_get_current_buf()
           local byte_size = vim.api.nvim_buf_get_offset(buf, vim.api.nvim_buf_line_count(buf))
-          if byte_size > 10 * 1024 * 1024 then -- 10 Megabyte max
+          if byte_size > 1024 * 1024 then -- 1 Megabyte max
             return {}
           end
-          return { buf }
-          -- return vim.api.nvim_list_bufs()
+
+          local bufs = {}
+          for _, win in ipairs(vim.api.nvim_list_wins()) do
+            local buf = vim.api.nvim_win_get_buf(win)
+            local byte_size = vim.api.nvim_buf_get_offset(buf, vim.api.nvim_buf_line_count(buf))
+            if byte_size < 1 * 1024 * 1024 then -- 1 Megabyte max
+              bufs[buf] = true
+            end
+          end
+          return vim.tbl_keys(bufs)
         end
       }
     },
