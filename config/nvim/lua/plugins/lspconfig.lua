a0b80582f9915326014a1229e15b23563151e0d8
SeonghwanSeo <shwan0106@gmail.com>
2024-05-21 19:33:51 +0900

    updategp

---
+++ config/nvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,33 @@
+local Util = require("lazyvim.util")
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      pyright = {
+        single_file_support = true,
+        setting = {
+          pyright = { disableLanguageServices = false, disableOrganizeImports = false },
+          python = {
+            analysis = {
+              diagnosticMode = "workspace",
+              typeCheckingMode = "standard",
+              diagnosticSeverityOverrides = {
+                reportImplicitStringConcatenation = "none",
+                reportGeneralTypeIssues = "warning",
+                reportDeprecated = "warning",
+                reportUnusedVariable = "none",
+                reportUnusedImport = "none",
+              },
+            }
+          },
+        },
+        ruff_lsp = {
+          single_file_support = true,
+          ruff = {
+
+          }
+        }
+      },
+    },
+  },
+}
